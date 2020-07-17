13fae5feb698d60b7df5a2635911edf6e03c86e4
Michael Cote <michaelcote7@outlook.com>
2020-07-16 17:00:18 -0700

    .

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App exicuted")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to my store!</Text>
       <StatusBar style="auto" />
     </View>
   );
