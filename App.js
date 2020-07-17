aa8233001f5ea71edef569d38dd592ad31d89230
Michael Cote <michaelcote7@outlook.com>
2020-07-16 17:44:07 -0700

    .

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 1;
   console.log("App exicuted")
   return (
     <View style={styles.container}>
